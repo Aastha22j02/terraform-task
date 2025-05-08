311d32a386122d6802f47203cc477e72c828377a
Aastha Gupta <102406428+Aastha22j02@users.noreply.github.com>
2025-05-08 18:40:08 +0530

    Update variables.tf

--- terraform-vpc-modules/modules/vpc/variables.tf
+++ terraform-vpc-modules/modules/vpc/variables.tf
@@ -3,3 +3,9 @@ variable "public_subnets" {}
 variable "private_subnets" {}
 variable "availability_zones" {}
 variable "enable_dns_hostnames" {}
+# variable "vpc_name" {}
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
